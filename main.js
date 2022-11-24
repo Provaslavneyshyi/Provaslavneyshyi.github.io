98619af4fa57bb08ab5fa91a4b46f7957f314cc2
Provaslavneyshyi <tutinin03@mail.ru>
2022-11-25 03:39:47 +0600

    complete animations

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
+AOS.init();
+
+// You can also pass an optional settings object
+// below listed default settings
+AOS.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 50, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 900, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
+
+
 const header = document.querySelector(".header");
 const about = document.querySelector(".about");
 const menu__active = document.querySelectorAll(".menu__item");
@@ -39,10 +67,8 @@ menu__active.forEach(function(item){
 
          if (scrollPosition > 842 && scrollPosition < 1800){
          about__link.classList.add('menu__item_active');
-         about__animation.classList.add('avatar__animation');
          } else {
          about__link.classList.remove('menu__item_active');
-         about__animation.classList.remove('avatar__animation');
          }
 
          if (scrollPosition > 1800 && scrollPosition < 2620){
